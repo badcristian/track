af60bd4f0e22455972996e12fd789c5a67c97376
badcristian <67512817+badcristian@users.noreply.github.com>
2023-07-05 10:04:35 -0700

    populated the main seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,20 +3,58 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Address;
+use App\Models\Comment;
+use App\Models\Company;
+use App\Models\Shipment;
+use App\Models\ShipmentStop;
+use App\Models\User;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\App;
 
 class DatabaseSeeder extends Seeder
 {
-    /**
-     * Seed the application's database.
-     */
+
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
+        if (!App::environment('production')) {
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+            Shipment::factory()
+                ->create()
+                ->each(function ($shipment) {
+
+                $users = User::factory(2)
+                    ->for(Company::factory())
+                    ->create();
+
+                $shipmentStops = ShipmentStop::factory(3)->state([
+                    'address_id' => fn() => Address::factory()->create(),
+                    'shipment_id' => $shipment->id
+                ])->create();
+
+                $users->each(function ($user) use ($shipment){
+                    $user->comments()->save(Comment::factory()->state([
+                        'user_id' => $user->id,
+                        'shipment_id' => $shipment->id
+                    ])->create());
+                });
+
+                $shipment->users()->attach($users);
+                $shipment->stops()->saveMany($shipmentStops);
+            });
+
+
+//
+//            User::factory()
+//                ->count(2)
+//                ->for(Company::factory())
+//                ->create();
+
+//            Company::factory()
+//                ->count(2)
+//                ->has(User::factory()->count(2))
+//                ->create();
+
+        }
     }
 }
