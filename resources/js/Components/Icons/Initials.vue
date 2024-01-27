3041dfb2e007355df1f3fe95d4b3152475afa7ed
badcristian <67512817+badcristian@users.noreply.github.com>
2024-01-27 15:20:08 -0800

    fe: add various svg icons

---
+++ resources/js/Components/Icons/Initials.vue
@@ -0,0 +1,46 @@
+<script setup>
+import {computed} from "vue";
+
+const props = defineProps({
+  name: String,
+  newMember: Boolean
+})
+
+const initials = computed(() => {
+  let init = ''
+  props.name.split(' ').forEach((el, index) => index <=1 ? init += el[0] : null)
+  return init
+})
+
+const randomColor = computed(()=>{
+  const colors = ['bg-slate-800', 'bg-slate-700', 'bg-slate-600', 'bg-gray-800', 'bg-gray-700', 'bg-gray-600'];
+  const nb = Math.floor(Math.random() * colors.length)
+  return colors[nb]
+})
+
+</script>
+
+<template>
+
+
+ <div>
+   <div v-if="!newMember"
+        class="rounded-3xl border border-gray-500 w-7 h-7 flex justify-center items-center truncate
+               hover:cursor-pointer text-sm"
+        :class="randomColor"
+   >
+     {{ initials }}
+   </div>
+
+   <div v-if="newMember" class="rounded-3xl border border-gray-500 border-dashed w-7 h-7 flex justify-center items-center truncate
+               hover:cursor-pointer text-sm"
+        >
+     +
+   </div>
+ </div>
+
+</template>
+
+<style scoped>
+
+</style>
---
+++ resources/js/Components/Icons/StatusIcon.vue
@@ -0,0 +1,32 @@
+<script setup>
+import SvgStatusDone from "@/Pages/Shipment/Partials/Icons/SvgStatusDone.vue";
+import {computed} from "vue";
+import {randomIntFromInterval} from "@/helpers.js";
+import SvgStatusCancelled from "@/Pages/Shipment/Partials/Icons/SvgStatusCancelled.vue";
+import SvgStatusInProgress from "@/Pages/Shipment/Partials/Icons/SvgStatusInProgress.vue";
+
+defineProps({
+    status: String
+})
+
+let rand = computed(()=>{
+    return randomIntFromInterval(0,2)
+})
+
+</script>
+
+<template>
+    <div class="group relative z-30">
+        <svg-status-done v-show="rand===0" class="w-7 " />
+        <svg-status-cancelled v-show="rand===1" class="w-7 " />
+        <svg-status-in-progress v-show="rand===2" class="w-7 " />
+
+<!--        <div class="hidden md:block p-1 rounded-md border border-gray-500">-->
+<!--            {{rand == 0 ? status.toUpperCase() : rand == 1 ? 'CANCELLED' : 'DONE'}}-->
+<!--        </div>-->
+        <span class="absolute z-20 bg-gray-700 rounded-lg p-3 text-sm whitespace-nowrap w-max opacity-0
+                    transition-opacity group-hover:opacity-100 shadow-xl shadow-black/10">
+            {{ status }}
+        </span>
+    </div>
+</template>
