2076a91c34c11d9eaaa5bec0d06c6d743452b6ba
badcristian <67512817+badcristian@users.noreply.github.com>
2024-01-29 13:14:38 -0800

    be: add routes

--- routes/web.php
+++ routes/web.php
@@ -1,38 +1,65 @@
 <?php
 
+use App\Http\Controllers\CommentController;
+use App\Http\Controllers\MemberController;
 use App\Http\Controllers\ProfileController;
-use Illuminate\Foundation\Application;
+use App\Http\Controllers\ShipmentController;
+use App\Http\Controllers\StopController;
 use Illuminate\Support\Facades\Route;
-use Inertia\Inertia;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return inertia('Welcome');
 });
-
-Route::get('/dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/dashboard', fn() => inertia('Dashboard/Index'))
+        ->name('dashboard.index');
+
+    Route::get('/stats', fn() => inertia('Dashboard/Index'))
+        ->name('stats');
+
+    Route::get('/profile', [ProfileController::class, 'edit'])
+        ->name('profile.edit');
+
+    Route::patch('/profile', [ProfileController::class, 'update'])
+        ->name('profile.update');
+
+    Route::delete('/profile', [ProfileController::class, 'destroy'])
+        ->name('profile.destroy');
+
+    Route::resource('shipments', ShipmentController::class);
+
+    Route::middleware('can:alter-shipment,shipment')
+        ->resource('shipments.stops', StopController::class)->names([
+            'store' => 'stops.store',
+            'update' => 'stops.update',
+            'destroy' => 'stops.destroy'
+        ])->only(['store', 'update', 'destroy']);
+
+    Route::middleware('can:alter-members,shipment')->group(function () {
+
+        Route::get('/shipments/{shipment}/members', [MemberController::class, 'index'])
+            ->name('members.search');
+
+        Route::post('/shipments/{shipment}/members', [MemberController::class, 'store'])
+            ->name('members.store');
+
+        Route::delete('/shipments/{shipment}/members', [MemberController::class, 'destroy'])
+            ->name('members.destroy');
+    });
+
+    Route::middleware('can:alter-shipment,shipment')->group(function () {
+
+        Route::post('/shipments/{shipment}/comments', [CommentController::class, 'update'])
+            ->name('comments.update');
+
+        Route::post('/shipments/{shipment}/comments', [CommentController::class, 'destroy'])
+            ->name('comments.destroy');
+
+        Route::post('shipments//{shipment}/comments', [CommentController::class, 'store'])
+            ->name('comments.store');
+    });
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
