bc7b6ba789d76c2ae9f8f09f913a5459d5b281c5
badcristian <67512817+badcristian@users.noreply.github.com>
2024-01-29 12:50:02 -0800

    be: add CommentController - wip

--- app/Http/Controllers/CommentController.php
+++ app/Http/Controllers/CommentController.php
@@ -2,9 +2,35 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Comment;
+use App\Models\Shipment;
+use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Gate;
 
 class CommentController extends Controller
 {
-    //
+    public function store(Request $request, Shipment $shipment): RedirectResponse
+    {
+        Gate::authorize('alter-shipment', $shipment);
+
+        $validated = $request->validate([
+            'body' => ['required', 'string', 'max:256'],
+            'user_id' => ['required', 'exists:users,id']
+        ]);
+
+        $shipment->comments()->create($validated);
+
+        return redirect()->back()->with('flash.banner', 'Comment added');
+    }
+
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    public function destroy(string $id)
+    {
+        //
+    }
 }
