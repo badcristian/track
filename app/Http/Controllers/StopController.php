5f00b12b6b70e3115841fd6339f8bb2483718ac4
badcristian <67512817+badcristian@users.noreply.github.com>
2024-01-29 12:51:07 -0800

    be: add StopController

--- app/Http/Controllers/StopController.php
+++ app/Http/Controllers/StopController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Http\Requests\StoreStopRequest;
+use App\Models\Shipment;
+use App\Models\Stop;
+use Illuminate\Support\Facades\Gate;
 
 class StopController extends Controller
 {
-    //
+    public function store(StoreStopRequest $request, Shipment $shipment)
+    {
+        $shipment->stops()->create($request->validated());
+
+        return redirect()->back()->with('flash.banner', 'Stop added');
+    }
+
+    public function update(StoreStopRequest $request, Shipment $shipment, Stop $stop)
+    {
+        $shipment->fill($request->except('id', 'created_at'));
+
+        $shipment->save();
+
+        return redirect()->back()->with('flash.banner', "Stop updated");
+    }
+
+    public function destroy(Shipment $shipment, Stop $stop)
+    {
+        $stop->delete();
+
+        return redirect()->back()->with('flash.banner', "Stop destroyed");
+    }
 }
